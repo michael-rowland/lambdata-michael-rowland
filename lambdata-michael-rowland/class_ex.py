584cc3d2c3ab21a46472a1da57634ef313ee2308
Michael Rowland <mrowland13@gmail.com>
2020-02-12 22:18:42 -0800

    0212 PM

--- lambdata-michael-rowland/class_ex.py
+++ lambdata-michael-rowland/class_ex.py
@@ -1,17 +1,13 @@
-
-class Batter(object):
-    def __init__(self, name, position, stance, hits, at_bats):
+# PLAYER CLASS
+# read about scope
+class Player():
+    def __init__(self, name, number, position, handedness):
         self.name = name
+        self.number = number
         self.position = position
-        self.stance = stance
-        self.hits = hits
-        self.at_bats = at_bats
+        self.handedness = handedness
 
-    @property #REVIEW THIS
-    def batting_average(self):
-        return self.hits / self.at_bats
-
-    @property
+    # @property
     def position_map(self):
         pmap = {
             1: 'P', 2: 'C', 3: '1B',
@@ -20,39 +16,65 @@ class Batter(object):
             }
         return pmap[self.position]
 
+    def box_score(self):
+        return f'{self.name} ({self.handedness}) - #{self.number} - {self.position_map()}'
+
+
+class Batter(Player):
+    def __init__(self, name, number, position, handedness, hits, at_bats):
+        super().__init__(name, number, position, handedness)
+        self.hits = hits
+        self.at_bats = at_bats
+
+    def batting_average(self):
+        return self.hits / self.at_bats
+
     def player_statistics(self):
-        print(f'{self.name}, {self.position_map}, {self.stance}')
-        print(f'Batting Average: {self.batting_average:.3f}')
+        print(f'Batting Average: {self.batting_average():.3f}')
 
-    @staticmethod
-    def advertise_generic():
-        print(f"Our team has the best players!")
+    # @staticmethod
+    # def advertise_generic():
+    #     print(f"Our team has the best players!")
 
-class Pitcher(object):
-    def __init__(self, name, position, handedness):
-        super().__init__(name, position)
-        self.handedness = handedness
+class Pitcher(Player):
+    def __init__(self, name, number, position, handedness, wins, starts):
+        super().__init__(name, number, position, handedness)
+        self.wins = wins
+        self.starts = starts
+
+    def win_pct(self):
+        return self.wins / self.starts
 
     def advertise(self):
-        print(f'{self.handedness} Handed Starting Pitcher: {self.name}')
+        print(f'Win Percentage: {self.win_pct()*100:.0f}%')
 
 if __name__ == "__main__":
 
     players = [
-        {'name': 'Byron Buxton', 'position': 8, 'stance': 'R', 'hits': 100, 'at_bats': 350},
-        {'name': 'Max Kepler', 'position': 9, 'stance': 'L', 'hits': 125, 'at_bats': 350},
-        {'name': 'Miguel Sano', 'position': 3, 'stance': 'R', 'hits': 150, 'at_bats': 350}
+        {'name': 'Byron Buxton', 'number': 25, 'position': 8, 
+        'handedness': 'R', 'hits': 100, 'at_bats': 350},
+        {'name': 'Max Kepler', 'number': 26, 'position': 9, 
+        'handedness': 'L', 'hits': 125, 'at_bats': 350},
+        {'name': 'Miguel Sano', 'number': 22, 'position': 3, 
+        'handedness': 'R', 'hits': 150, 'at_bats': 350}
     ]
     pitchers = [
-        {'name': 'Jose Berrios', 'position': 1, 'handedness': 'Right'},
-        {'name': 'Lewis Thorpe', 'position': 1, 'handedness': 'Left'}
+        {'name': 'Jose Berrios', 'position': 1, 'number': 17, 
+        'handedness': 'R', 'wins': 20, 'starts': 30},
+        {'name': 'Lewis Thorpe', 'position': 1, 'number': 43, 
+        'handedness': 'L', 'wins': 5, 'starts': 15}
     ]
 
     for p in players:
-        player = Batter(name=p['name'], position=p['position'], 
-        stance=p['stance'], hits=p['hits'], at_bats=p['at_bats'])
-        player.player_statistics()
+        player_b = Batter(name=p['name'], position=p['position'], 
+        number=p['number'], handedness=p['handedness'], hits=p['hits'], 
+        at_bats=p['at_bats'])
+        print(player_b.box_score())
+        player_b.player_statistics()
     
-    # for p in pitchers:
-    #     thrower_of_the_baseball = Pitcher(name=p['name'], position=p['position'], handedness=p['handedness'])
-    #     thrower_of_the_baseball.advertise()
+    for p in pitchers:
+        player_p = Pitcher(name=p['name'], position=p['position'], 
+        number=p['number'], handedness=p['handedness'], wins=p['wins'], 
+        starts=p['starts'])
+        print(player_p.box_score())
+        player_p.advertise()
